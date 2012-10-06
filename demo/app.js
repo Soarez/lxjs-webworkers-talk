c65445b998c76229216f0c53961eeb78b9ae07a5
Soarez <igorsoarez@gmail.com>
2012-10-06 15:40:49 +0100

    Remove clutter

--- demo/app.js
+++
@@ -1,9 +0,0 @@
-var Static = require('node-static')
-  , file   = new(Static.Server)('./');
-
-require('http').createServer(function (request, response) {
-  request.addListener('end', function () {
-    file.serve(request, response);
-  });
-}).listen(8081);
-console.log("im on 8081");
